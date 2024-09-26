2eca21ac6d0e78ec7f4760d6b5c8c03cafa6dac0
aarav-gargg <aaravgarg975@gmail.com>
2024-09-26 18:36:14 +0530

    "Added new routes and controllers for wishlist functionality"

--- BackEnd/Routes/user.route.js
+++ BackEnd/Routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getTripList , getProperty } from "../controller/user.controller.js";
+import { getTripList , getProperty , addToWishList , clearWishList , getWishList} from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.get("/:userId/trips" , getTripList);
 router.get("/property/:propertyId",getProperty)
+router.get("/wishList",addToWishList)
+router.get("/:userId/wishList",getWishList)
 
 export default router;
--- BackEnd/controller/user.controller.js
+++ BackEnd/controller/user.controller.js
@@ -30,11 +30,81 @@ export const getProperty = async(req,res,next) => {
     }
 }
 
-export const getWishList = async(req,res,next) => {
+export const getWishList = async (req, res, next) => {
     try {
-        
+        const { userId } = req.params; 
+
+       
+        const user = await User.findById(userId).populate('wishList'); 
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(user.wishList);
     } catch (error) {
-        
+        next(error);
     }
-}
+};
+
+
+export const addToWishList = async (req, res, next) => {
+    try {
+        const { propertyId, userId } = req.body;
+        const property = await Host.findById(propertyId);
+
+        if (!property) {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPropertyInWishList = user.wishList.some(
+            (wishlistItem) => wishlistItem._id.toString() === property._id.toString()
+        );
+
+        if (isPropertyInWishList) {
+            return res.status(400).json({ message: 'Property already in wishlist' });
+        }
+ 
+        await User.updateOne(
+            { _id: userId },
+            { $push: { wishList: property } }  
+        );
+
+        const updatedUser = await User.findById(userId);
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const clearWishList = async (req, res, next) => {
+    try {
+        const { userId } = req.body;
+
+        
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+      
+        await User.updateOne(
+            { _id: userId },
+            { $set: { wishList: [] } }
+        );
+
+        const updatedUser = await User.findById(userId);
+        res.status(200).json({ message: 'WishList cleared', user: updatedUser });
+    } catch (error) {
+        next(error);
+    }
+};
+
 
